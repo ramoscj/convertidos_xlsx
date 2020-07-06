e4b5fc63dc599a6d84054015dcfa07dbe940a988
Carlos Ramos <ramoscj@users.noreply.github.com>
2020-07-06 17:29:40 -0400

    Carlos Ramos
    Refactorizando el codigo del proceso.

--- crear_xlsx.py
+++ crear_xlsx.py
@@ -4,9 +4,36 @@ from tqdm.auto import tqdm
 import os.path
 import csv
 
-def crear_csv():
+def leer_xlsx(archivo):
+    try:
+        data_txt = []
+        # read_only=True para leer el archivo y consumir menos recursos
+        # data_only=True para leer el valor de las celdas que tiene formulas
+        xls = load_workbook(archivo, read_only=True, data_only=True)
+        nombre_hoja = xls.sheetnames
+        hoja = xls[nombre_hoja[0]]
+        cantidad_columnas = len(tuple(hoja.rows))
+        for columna in tqdm(iterable=hoja.rows, total = cantidad_columnas, desc='Leyendo DATA' , unit='Row'):
+            data_xls = []
+            for fila in columna:
+                data_xls.append(fila.value)
+            data_txt.append(data_xls)
+        return data_txt
+    except Exception as e:
+        raise Exception('Error al leer archivo: %s | %s' % (archivo, e))
+
+def escribir_txt(archivo_salida, data_txt):
+    try:
+        with open(archivo_salida, 'w', newline='') as txt:
+            writer = csv.writer(txt, delimiter=';')
+            for data in tqdm(iterable=data_txt, total = len(data_txt), desc='Escribiendo DATA', unit='Row'):
+                writer.writerow(data)
+        return True
+    except Exception as e:
+        raise Exception('Error al escribir archivo: %s | %s' % (archivo_salida, e))
+
+def crear_txt():
     archivo = 'test.xlsx'
-    data_txt = []
     archivo_existe = os.path.isfile(archivo)
     salida_txt = 'prueba.txt'
     print("Buscando archivo: %s" % archivo)
@@ -14,25 +41,16 @@ def crear_csv():
     if archivo_existe:
         print("Archivo: %s encontrado." % archivo)
         print("Iniciando Lectura...")
-        xls = load_workbook(archivo, read_only=True, data_only=True)
-        nombre_hoja = xls.sheetnames
-        hoja = xls[nombre_hoja[0]]
-        cantidad_columnas = len(tuple(hoja.rows))
-        for columna in tqdm(iterable=hoja.rows, total = cantidad_columnas, desc='Leyendo DATA ' , unit='Row'):
-            data_xls = []
-            for fila in columna:
-                data_xls.append(fila.value)
-            data_txt.append(data_xls)
-        with open(salida_txt, 'w', newline='') as txt:
-            writer = csv.writer(txt, delimiter=';')
-            for data in tqdm(iterable=data_txt, total = len(data_txt), desc='Escribiendo DATA ', unit='Row'):
-                writer.writerow(data)
-        print("Archivo Creado !!")
+        data_txt = leer_xlsx(archivo)
+        if escribir_txt(salida_txt, data_txt):
+            print("Archivo Creado !!")
+        else:
+            print("Error!!")
         return True
     else:
         return False
 
-crear_csv()
+crear_txt()
 
 
 
